e6b0e3a58e331793af976a8e83aae6f1f20c395f
Poramat Wongtawan <poramatw.kla@gmail.com>
2022-04-08 14:25:16 +0700

    stateless column center text

--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,49 @@ class Fluttless extends StatelessWidget {
       title: "Mtest",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Hello Flutter"),
+          title: Text("MtestFX"),
         ),
         body: Center(
-          child: Text("Hello World"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              Image(
+                  image: NetworkImage(
+                      "https://uxwing.com/wp-content/themes/uxwing/download/42-business-professional-services/trade.png")),
+              Text(
+                "MtestFX",
+                style: TextStyle(fontSize: 40, color: Colors.green),
+              ),
+              Text("MtestFX LOW SPREAD",
+                  style: TextStyle(fontSize: 15, color: Colors.green)),
+              Text("สามารถส่งคำสั่งได้ตลอด ราคาส่งตรงจาก LP",
+                  style: TextStyle(fontSize: 15, color: Colors.green)),
+              Text("ทดลองเทรดฟรี ฝากเงินเพื่อรับโบนัส 30%",
+                  style: TextStyle(fontSize: 15, color: Colors.green)),
+              Text("สูงสุดถึง 1000 และโปรโมชั่นอีกมากมาย",
+                  style: TextStyle(fontSize: 15, color: Colors.green)),
+              Text("เพียงแค่เทรดกับเรา",
+                  style: TextStyle(fontSize: 15, color: Colors.green))
+            ],
+          ),
         ),
       ),
       theme: ThemeData(primarySwatch: Colors.cyan),
     );
+  }
+}
+
+class Fluttful extends StatefulWidget {
+  const Fluttful({Key? key}) : super(key: key);
+
+  @override
+  State<Fluttful> createState() => _FluttfulState();
+}
+
+class _FluttfulState extends State<Fluttful> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
