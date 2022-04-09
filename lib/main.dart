a168383d589c87ed3a4511b1fd327e9ccb257c2f
Poramat Wongtawan <poramatw.kla@gmail.com>
2022-04-09 13:51:11 +0700

    add container padding Text

--- lib/main.dart
+++ lib/main.dart
@@ -31,19 +31,50 @@ class _FluttfulState extends State<Fluttful> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-              color: Colors.indigo, borderRadius: BorderRadius.circular(25)),
-          height: 300,
+        body: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                padding: const EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 247, 176, 83),
+                    borderRadius: BorderRadius.circular(25)),
+                height: 200,
+                width: 300,
+                child: Column(
+                  children: const [
+                    Text(
+                      "MT4 Standard Account",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                    Text(
+                      "1234567890",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                    Text(
+                      "2,000.00 USD",
+                      style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
-        Container(
-          decoration: BoxDecoration(
-              color: Colors.indigo, borderRadius: BorderRadius.circular(25)),
-          height: 300,
-        )
-      ],
+      ),
     ));
   }
 }
